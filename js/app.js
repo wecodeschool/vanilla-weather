6fec7d3469bfab3103cffa2b4b090b7c9672787e
Matt Dellac <matt@dellac.org>
2018-10-25 21:29:23 +0100

    Add isWindy function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-// alert('Hello World');
+function isWindy(speed) {
+  return speed > 5;
+}
+
+let lisbonSpeed = 10;
+
+if (isWindy(lisbonSpeed)) {
+  alert('it is windy');
+} else {
+  alert('it is not windy');
+}
+
