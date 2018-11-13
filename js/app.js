d5fc81c97a2c68cb538daae2267937d5ab31ee51
Matt Dellac <matt@dellac.org>
2018-11-13 18:14:59 +0000

    add js

---
+++ js/app.js
@@ -0,0 +1,15 @@
+let selected = document.querySelector('.day__block--selected');
+
+function showTemperature() {
+  document.querySelector('.weather-temp').innerHTML = this.getAttribute('max');
+  document.querySelector('.weather__icon--today').setAttribute("src", 'images/' + this.getAttribute('conditions') + '.png');
+
+  selected.classList.remove('day__block--selected');
+  selected = this;
+  selected.classList.add('day__block--selected');
+}
+
+let elements = document.querySelectorAll('.day__block');
+elements.forEach(function(element, index) {
+  element.addEventListener('click', showTemperature);
+});
