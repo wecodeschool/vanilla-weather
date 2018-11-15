9d662e549cd23bcc7585d5816598c2705d10dead
Matt Dellac <matt@dellac.org>
2018-11-15 18:02:31 +0000

    class5

--- js/app.js
+++ js/app.js
@@ -1,17 +1,22 @@
-let selected  = document.querySelector('.day__block--selected');
-let days      = document.querySelectorAll('.day__block');
-let todayTemp = document.querySelector('.weather-temp--today');
-let todayIcon = document.querySelector('.weather__icon--today');
+let selected      = document.querySelector('.day__block--selected');
+let days          = document.querySelectorAll('.day__block');
+let temperature   = document.querySelector('.weather-temp--today');
+let wind          = document.querySelector('#wind-speed');
+let precipitation = document.querySelector('#precipitation-probality');
+let description   = document.querySelector('#weather-description');
+let place         = document.querySelector('#weather-location');
+let icon          = document.querySelector('.weather__icon--today');
 
-function refreshTemp(event) {
-  todayTemp.innerHTML = event.currentTarget.getAttribute('max');
-  document.querySelector('.day__block--selected').classList.remove('day__block--selected');
-  event.currentTarget.classList.add('day__block--selected');
 
-  let iconPath = 'images/' + event.currentTarget.getAttribute('conditions') + '.png'
-  todayIcon.setAttribute('src', iconPath);
-};
+// function refreshTemp(event) {
+//   todayTemp.innerHTML = event.currentTarget.getAttribute('max');
+//   document.querySelector('.day__block--selected').classList.remove('day__block--selected');
+//   event.currentTarget.classList.add('day__block--selected');
 
-days.forEach(function(day, index) {
-  day.addEventListener('click', refreshTemp);
-});
+//   let iconPath = 'images/' + event.currentTarget.getAttribute('conditions') + '.png'
+//   todayIcon.setAttribute('src', iconPath);
+// };
+
+// days.forEach(function(day, index) {
+//   day.addEventListener('click', refreshTemp);
+// });
