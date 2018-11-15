5683e3412ebd21d222cb2bc25ace554bf0663c02
Matt Dellac <matt@dellac.org>
2018-11-15 18:00:38 +0000

    wip

--- js/app.js
+++ js/app.js
@@ -1,17 +1,37 @@
-let selected  = document.querySelector('.day__block--selected');
-let days      = document.querySelectorAll('.day__block');
-let todayTemp = document.querySelector('.weather-temp--today');
-let todayIcon = document.querySelector('.weather__icon--today');
+let selected      = document.querySelector('.day__block--selected');
+let days          = document.querySelectorAll('.day__block');
+let temperature   = document.querySelector('.weather-temp--today');
+let wind          = document.querySelector('#wind-speed');
+let precipitation = document.querySelector('#precipitation-probality');
+let description   = document.querySelector('#weather-description');
+let place         = document.querySelector('#weather-location');
+let icon          = document.querySelector('.weather__icon--today');
 
-function refreshTemp(event) {
-  todayTemp.innerHTML = event.currentTarget.getAttribute('max');
-  document.querySelector('.day__block--selected').classList.remove('day__block--selected');
-  event.currentTarget.classList.add('day__block--selected');
+let root   = 'https://api.openweathermap.org';
+let apiKey = '5f472b7acba333cd8a035ea85a0d4d4c';
+let path   = 'data/2.5/weather';
+let city   = 'Lisbon';
 
-  let iconPath = 'images/' + event.currentTarget.getAttribute('conditions') + '.png'
-  todayIcon.setAttribute('src', iconPath);
-};
+axios.get(root + '/' + path + '?q=' + city + '&appid=' + apiKey + '&units=metric')
+  .then(function(response) {
+    // console.log(response.data)
+    place.innerHTML         = response.data.name;
+    description.innerHTML   = response.data.weather[0].main;
+    temperature.innerHTML   = Math.round(response.data.main.temp);
+    wind.innerHTML          = Math.round(response.data.wind.speed);
+    precipitation.innerHTML = Math.round(response.data.main.humidity);
+    icon.setAttribute('src', 'http://openweathermap.org/img/w/' + response.data.weather[0].icon + '.png')
+  });
 
-days.forEach(function(day, index) {
-  day.addEventListener('click', refreshTemp);
-});
+
+  // https://api.openweathermap.org/data/2.5/forecast?q=Lisbon&units=metric&appid=5f472b7acba333cd8a035ea85a0d4d4c
+path = 'data/2.5/forecast';
+axios.get(root + '/' + path + '?q=' + city + '&appid=' + apiKey + '&units=metric')
+  .then(function(response) {
+    document.querySelectorAll('.day__block').forEach(function(element, index) {
+      element.querySelector('.day__block-temp--max').innerHTML = Math.round(response.data.list[index].main.temp_max);
+      element.querySelector('.day__block-temp--min').innerHTML = Math.round(response.data.list[index].main.temp_min);
+      element.querySelector('.day__block-image').setAttribute('src', 'http://openweathermap.org/img/w/' + response.data.list[index].weather[0].icon + '.png')
+    });
+  });
+
