2f82f3407212fb9744fe375309a98ce62bae3c96
Matt Dellac <matt@dellac.org>
2018-11-15 16:04:16 +0000

    refactoring

--- js/app.js
+++ js/app.js
@@ -3,12 +3,12 @@ let days      = document.querySelectorAll('.day__block');
 let todayTemp = document.querySelector('.weather-temp--today');
 let todayIcon = document.querySelector('.weather__icon--today');
 
-function refreshTemp() {
-  todayTemp.innerHTML = this.getAttribute('max');
+function refreshTemp(event) {
+  todayTemp.innerHTML = event.currentTarget.getAttribute('max');
   document.querySelector('.day__block--selected').classList.remove('day__block--selected');
-  this.classList.add('day__block--selected');
+  event.currentTarget.classList.add('day__block--selected');
 
-  let iconPath = 'images/' + this.getAttribute('conditions') + '.png'
+  let iconPath = 'images/' + event.currentTarget.getAttribute('conditions') + '.png'
   todayIcon.setAttribute('src', iconPath);
 };
 
