a6bf3c6b5550d6e6283f1815b50dddde8da57319
Matt Dellac <matt@dellac.org>
2018-11-20 16:18:49 +0000

    add geolocation api

--- js/app.js
+++ js/app.js
@@ -10,8 +10,6 @@ let wind          = document.querySelector('#wind-speed');
 let root   = 'https://api.openweathermap.org';
 let apiKey = '5f472b7acba333cd8a035ea85a0d4d4c';
 let path   = 'data/2.5/weather';
-let city   = 'Lisbon';
-
 
 function friendlyDate(date) {
   let days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
@@ -21,26 +19,32 @@ function friendlyDate(date) {
   return days[date.getDay() - 1] + ' ' + date.getHours() + ":" + minutes;
 };
 
-axios.get(root + '/' + path + '?q=' + city + '&appid=' + apiKey + '&units=metric')
-.then(function(response) {
-    date.innerHTML = friendlyDate(new Date());
-    place.innerHTML         = response.data.name;
-    description.innerHTML   = response.data.weather[0].main;
-    temperature.innerHTML   = Math.round(response.data.main.temp);
-    wind.innerHTML          = Math.round(response.data.wind.speed);
-    precipitation.innerHTML = Math.round(response.data.main.humidity);
-    icon.setAttribute('src', 'http://openweathermap.org/img/w/' + response.data.weather[0].icon + '.png')
-  });
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function (position) {
+    console.log(position.coords)
 
-path = 'data/2.5/forecast';
-axios.get(root + '/' + path + '?q=' + city + '&appid=' + apiKey + '&units=metric')
-  .then(function(response) {
-    console.log(response.data);
-    document.querySelectorAll('.day__block').forEach(function(element, index) {
-      let day = new Date(response.data.list[index].dt_txt);
-      element.querySelector('.day__block-date').innerHTML = friendlyDate(day);
-      element.querySelector('.day__block-temp--max').innerHTML = Math.round(response.data.list[index].main.temp_max);
-      element.querySelector('.day__block-temp--min').innerHTML = Math.round(response.data.list[index].main.temp_min);
-      element.querySelector('.day__block-image').setAttribute('src', 'http://openweathermap.org/img/w/' + response.data.list[index].weather[0].icon + '.png')
-    });
+    axios.get(root + '/' + path + '?lat=' + Math.round(position.coords.latitude) + '&lon=' + Math.round(position.coords.longitude) + '&appid=' + apiKey + '&units=metric')
+    .then(function(response) {
+        date.innerHTML = friendlyDate(new Date());
+        place.innerHTML         = response.data.name;
+        description.innerHTML   = response.data.weather[0].main;
+        temperature.innerHTML   = Math.round(response.data.main.temp);
+        wind.innerHTML          = Math.round(response.data.wind.speed);
+        precipitation.innerHTML = Math.round(response.data.main.humidity);
+        icon.setAttribute('src', 'http://openweathermap.org/img/w/' + response.data.weather[0].icon + '.png')
+      });
+
+    path = 'data/2.5/forecast';
+    axios.get(root + '/' + path + '?lat=' + Math.round(position.coords.latitude) + '&lon=' + Math.round(position.coords.longitude) + '&appid=' + apiKey + '&units=metric')
+      .then(function(response) {
+        console.log(response.data);
+        document.querySelectorAll('.day__block').forEach(function(element, index) {
+          let day = new Date(response.data.list[index].dt_txt);
+          element.querySelector('.day__block-date').innerHTML = friendlyDate(day);
+          element.querySelector('.day__block-temp--max').innerHTML = Math.round(response.data.list[index].main.temp_max);
+          element.querySelector('.day__block-temp--min').innerHTML = Math.round(response.data.list[index].main.temp_min);
+          element.querySelector('.day__block-image').setAttribute('src', 'http://openweathermap.org/img/w/' + response.data.list[index].weather[0].icon + '.png')
+        });
+      });
   });
+}
